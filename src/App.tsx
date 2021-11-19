37f466fb9f8a46fcc8365ab88dbc508f1ce88d4f
Jasparr77 <jaspercroome@protonmail.com>
2021-11-19 06:38:38 -0800

    initial new portfolio push

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import Words from './Words';
+
+function App() {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Update the count (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
+  return (
+    <Words/>
+  );
+}
+
+export default App;
---
+++ src/Words.tsx
@@ -0,0 +1,80 @@
+import React, {useEffect} from 'react'
+
+const Words = () => {
+  
+  return (
+    <>
+    
+    <div>
+    <h1>
+    Hi, I'm Jasper Croome.
+    </h1>
+    <p>
+      I make delightful user experiences online through the use of modern web technologies.
+    </p>
+    <p>
+    <a href="https://observablehq.com/@jasper">Observable</a> | <a 
+    href="https://github.com/Jasparr77">Github</a> | <a 
+    href="mailto:jaspercroome@protonmail.com">email</a> | <a 
+    href="https://www.linkedin.com/in/jaspercroome/">LinkedIn</a> | <a 
+    href="https://twitter.com/crasperjoome?lang=en">Twitter</a>
+    </p>
+    </div>
+
+    <div>
+    <h4>
+      Work Examples
+    </h4>
+    <p>
+      Outside of work, I've had the oppourtunity to build some interesting things, like <br/><a 
+      href="https://benicer.vercel.app/">a 3D globe to motivate people to be nicer</a> and <a 
+      href="https://audioforma.vercel.app/#">a full-stack application to analyze the emotion in your spotify playlist</a>.
+      <br/>At work, you can believe me when I say that I build very cool things, all day long.
+    </p>
+    </div>
+
+    <div>
+    <h4>
+      Deepest Skills Today
+    </h4>
+    <p>
+      I'm a front-end dev at heart, which means a lot of what I do at work 
+      (and in my spare time) uses <br/>
+      <a href='https://reactjs.org'>React</a>, <a 
+      href='https://www.typescriptlang.org/'>TypeScript</a>, <a 
+      href='https://d3js.org/'>D3.js</a>, and <a 
+      href='https://www.adobe.com/creativecloud/ui-ux.html'>The Adobe Suite (Illustrator, XD, Photoshop)</a>.
+    </p>
+    </div>
+
+    <div>
+    <h4>Libraries and Frameworks I'm Learning</h4>
+    <p>
+      Nothing ever stays in place forever, so I'm also working to get up-to-speed with <br/>
+    <a href="https://kit.svelte.dev/">Svelte</a> & <a 
+    href="https://threejs.org/">Three.js</a>.
+    </p>
+    </div>
+
+    <div>
+    <h4>Other technologies</h4>
+    <p>
+      I'm proud of my t-shaped skillset, and there are some dev-adjacent skills I've picked up on my journey.
+      Namely, <b>
+        agile methodology</b>, <b>
+        analytics</b>, <b>
+        aws</b>, <b>
+        git cli</b>, <b>
+        Mentoring</b>, <b>
+        Tableau</b>, <b>
+        Testing (unit)</b>, <b>
+        User Interviewing & Research</b>, <b>
+        User Training</b>, 
+        and that's all I could think of for now.
+    </p>
+    </div>
+    </>
+  )
+}
+
+export default Words;
