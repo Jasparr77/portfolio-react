b4f64b4199e03d99903de1a9efb636a43ca270a2
Jasparr77 <jaspercroome@protonmail.com>
2021-11-28 09:37:47 -0800

    bouncing arrow

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -1,61 +1,84 @@
 import { useSpring, animated } from '@react-spring/web'
-import React, { useEffect, useState } from 'react'
+import React, { CSSProperties, useEffect, useMemo, useState } from 'react'
+import { useScroll } from 'react-use-gesture'
 import Words from './Words'
 
 
 const Cards = () =>{
-  const [cardOffset, setCardOffset] = useState(0)
   const springConfig = {
-    loop:true,
-    config:{
-      friction:1,
-      tension:100
-    }
+    loop: { reverse: true },
   }
   
-  const arrowStyles = useSpring({
-    from:{transform:'translate(0px, 20px)'},
-    to:[
-      {transform:'translate(0px, 0px)'},
-    ],
+  const arrowStyles = useSpring<CSSProperties>({
+    from:{
+      y:35,
+    },
+    to:{
+      y:15,
+    },
     ...springConfig
   })
+
+  const shadowStyles = useSpring<CSSProperties>({
+    to:{
+      opacity:'0.2',
+      cx:20,
+      cy:62.5,
+      rx:2,
+      ry:2
+    },
+    from:{
+      opacity:'1',
+      cx:20,
+      cy:62.5,
+      rx:15,
+      ry:4
+    },
+    ...springConfig
+  })
+
+  
+  const scrollClickHandler = (scrollPos:number)=>{
+    if (scrollPos <= document.body.scrollHeight - window.innerHeight){
+      window.scrollBy({top:window.innerHeight, behavior:'smooth'})
+      console.log(document.body.scrollHeight, window.scrollY)
+    } else window.scrollTo({top:0,behavior:'smooth'})
+  }
 
   useEffect(()=>{
     if (typeof document !== 'undefined'){
       const height = window.innerHeight - 30
-      const width = window.innerWidth
       const allCards = document.querySelectorAll('.wordContainer')
       allCards.forEach((card,cardIndex)=>{
         card.setAttribute('style',`
         height: ${height}px;
-        ease: transform 1s;
         `)
       })
     }
   })
+  
   return (
     <>
-    <animated.svg overflow="visible" width="100%" id="arrowContainer"
+    <animated.svg
+      overflow="visible"
+      x={`${innerWidth*.8}`} 
+      width="60px" 
+      height="80px" 
+      id="arrowContainer"
+      style={{cursor:'pointer'}}
+      onClick={()=>scrollClickHandler(window.scrollY)}
       >
+      <animated.ellipse
+        fill="lightgrey"
+        style={shadowStyles}
+        />
       <animated.path
-      d={
-        `M${innerWidth - 80},0 
-        L${innerWidth - 60},20 
-        L${innerWidth - 40},0`
-      } 
+      id="arrow"
+      d={`M0,0 L20,20 L40,0`} 
       stroke="rebeccapurple"
       strokeWidth="8px"
       fill="none"
       style={arrowStyles}
-      />
-      <animated.ellipse
-        cx={innerWidth - 60}
-        cy="65"
-        rx="15"
-        ry="5"
-        fill="lightgrey"
-        opacity=".2"
       />
     </animated.svg>
     <Words />
